ba56094eba38d5605d61b1b11bb4b096bd10c496
GZGavinZhao <74938940+GZGavinZhao@users.noreply.github.com>
2021-07-03 16:44:50 -0400

    Added grinder workflow "clean"

--- tool/grind.dart
+++ tool/grind.dart
@@ -1,15 +1,53 @@
+import 'dart:io';
+import 'package:git/git.dart';
 import 'package:grinder/grinder.dart';
+import 'package:path/path.dart' as p;
 
 main(args) => grind(args);
 
-@Task()
-test() => TestRunner().testAsync();
-
-@DefaultTask()
-@Depends(test)
+@Task('Build site')
 build() {
   Pub.build();
 }
 
-@Task()
-clean() => defaultClean();
+@DefaultTask()
+void usage() => print('Run `grind --help` to list available tasks.');
+
+/// By default this cleans every temporary directory and build artifacts
+/// Because `grinder` doesn't have negatable falgs yet, if you don't
+/// want to delete something, **PASS THAT THING** as a flag
+///
+/// For example, if you **DON'T** want to delete the "publish" folder,
+/// run `grind clean --publish`. It will clean everything else.
+@Task('Clean temporary directories and build artifacts')
+clean() async {
+  // Ask grinder to add an negtable option
+  TaskArgs args = context.invocation.arguments;
+
+  // Cleans the "publish" directory
+  bool cleanSite = !args.getFlag('publish');
+  if (cleanSite && Directory('publish').existsSync()) {
+    delete(Directory('publish'));
+  }
+
+	// Cleans the "$HOME/tmp" directory, used by some git stuffs
+  bool cleanTmp = !args.getFlag('tmp');
+  if (cleanTmp) {
+    String path = p.join(
+			// Please don't tell me you're on Android or iOS
+      Platform.isWindows
+          ? Platform.environment['UserProfile']
+          : Platform.environment['HOME'],
+      'tmp',
+    );
+    if (Directory(path).existsSync()) {
+			delete(Directory(path));
+    }
+  }
+
+	// Cleans the "src./asset-cache" directory
+	bool cleanAssetCache = !args.getFlag('assetcache');
+	if (cleanAssetCache && Directory('src/.asset-cache').existsSync()) {
+		delete(Directory('src/.asset-cache'));
+	}
+}
