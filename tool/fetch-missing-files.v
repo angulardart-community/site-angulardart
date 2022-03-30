fe4b33ce431337c58d963d4773bf714d93a5007f
Gavin Zhao <git@gzgz.dev>
2022-03-29 21:30:56 -0400

    Use submodule for dependency-injection
    Signed-off-by: Gavin Zhao <git@gzgz.dev>

---
+++ tool/fetch-missing-files.v
@@ -0,0 +1,27 @@
+import os
+import net.http
+
+fn main() {
+	url_base := 'https://raw.githubusercontent.com/angulardart-community/site-angulardart/master/examples/ng/doc/'
+
+	target_example := os.getwd().split('/').last()
+	println('Target example: $target_example')
+
+	if os.args.len < 2 {
+		println('Usage: <file-to-merge>')
+		exit(0)
+	}
+
+	for file in os.args[1..] {
+		if os.exists(file) {
+			eprintln('Warning: $file already exists. This will overwrite it.')
+			exit(1)
+		}
+
+		http.download_file(os.join_path(url_base, target_example, os.args[1]), file) ?
+
+		println('Downloaded $file')
+	}
+
+	// file := os.join_path(os.args[1])
+}
